10a439ec64d214e1a9e2be505871e9a773d2b34b
jordanfox1 <jordanfox9415@gmail.com>
2022-02-02 21:26:55 +1000

    created classes page basic layout

--- gyeme_react/src/components/classes/ClassesPage.jsx
+++ gyeme_react/src/components/classes/ClassesPage.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import yoga from './images/yoga.jpg'
+import functional from './images/functional.jpg'
+import flexing from './images/flexing.jpg'
+import "./classes_page.css"
 
 
 export default function ClassesPage() {
@@ -13,17 +16,17 @@ export default function ClassesPage() {
         },
         {
             name: "Functional Strength Training",
-            trainer: "the Hammer",
+            trainer: "Jacked Johnson",
             membersOnly: true,
             description: "Join us for this amazing class at 9pm on saturday the 3rd of october",
-            image: yoga
+            image: functional
         },
         {
             name: "Professional Flexing",
             trainer: "the Hammer",
-            membersOnly: true,
+            membersOnly: false,
             description: "Join us for this amazing class at 1am on saturday the 3rd of october",
-            image: yoga
+            image: flexing
         },
     ]
 
@@ -35,12 +38,17 @@ export default function ClassesPage() {
             </div>
             <section>
                 {classes.map((element) => (
-                    <div className='trainer'>
+                    <div className='class'>
                         <figure>
                             <figcaption><strong>{element.name}</strong></figcaption>
-                            <img className='display-picture' src={element.image} width={176} height={137} alt="Picture of class" />
-                            <a href="/">Signup for this class!</a>
+                            <img className='display-picture' src={element.image} width={300} height={320} alt="Picture of class" />
+                            <figcaption><a href="/">Sign up for this class!</a></figcaption>
                         </figure>
+                        
+                    <p>{element.description} hosted by <strong>{element.trainer}</strong></p>
+                    <button>book this class!</button>
+                    {element.membersOnly == true ? <p className='members-only'>members only</p> : <p className='members-only'>free to public!</p>}
+                    
                     </div>
                 ))}
             </section>
