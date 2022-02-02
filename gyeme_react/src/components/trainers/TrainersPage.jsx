aa7490dd05a1441b57cbf868c27ad65bd12a837a
jordanfox1 <jordanfox9415@gmail.com>
2022-02-02 16:56:12 +1000

    further styling and fixing trainers page

--- gyeme_react/src/components/trainers/TrainersPage.jsx
+++ gyeme_react/src/components/trainers/TrainersPage.jsx
@@ -6,27 +6,62 @@ import './trainers_page.css'
 //for each trainer: render the trainer div
 export default function TrainersPage() {
   return (
+  <>
+    <div className='heading'>
+        <h1>Trainers</h1>
+        <h4>View All available Gyeme trainers!</h4>
+    </div>
+
       <section>
-          <div className='heading'>
-            <h1>Trainers</h1>
-            <h4>View All available Gyeme trainers!</h4>
-          </div>
           <div className='trainer'>
             <figure>
                     <figcaption><strong>Big Ronny</strong></figcaption>
-                <img src={ronny} width={176} height={137} alt="" />
+                <img className='display-picture' src={ronny} width={176} height={137} alt="" />
                     <a href="/">Contact this trainer!</a>
             </figure>
             <div className='class-list'>
             <p>This trainer is the reigning WWE champion and has a lat-span of 1.5m</p>
-                <h3>Classes</h3>
+                <h3 className='classlist'>Classes</h3>
                 <ul>
                     <li>Bodybuilding</li>
                     <li>Powerlifting</li>
                     <li>Jujitsu</li>
                 </ul>
-            </div>
+            </div> 
+          </div>
+          <div className='trainer'>
+            <figure>
+                    <figcaption><strong>Big Ronny</strong></figcaption>
+                <img className='display-picture' src={ronny} width={176} height={137} alt="" />
+                    <a href="/">Contact this trainer!</a>
+            </figure>
+            <div className='class-list'>
+            <p>This trainer is the reigning WWE champion and has a lat-span of 1.5m</p>
+                <h3 className='classlist'>Classes</h3>
+                <ul>
+                    <li>Bodybuilding</li>
+                    <li>Powerlifting</li>
+                    <li>Jujitsu</li>
+                </ul>
+            </div> 
+          </div>
+          <div className='trainer'>
+            <figure>
+                    <figcaption><strong>Big Ronny</strong></figcaption>
+                <img className='display-picture' src={ronny} width={176} height={137} alt="" />
+                    <a href="/">Contact this trainer!</a>
+            </figure>
+            <div className='class-list'>
+            <p>This trainer is the reigning WWE champion and has a lat-span of 1.5m</p>
+                <h3 className='classlist'>Classes</h3>
+                <ul>
+                    <li>Bodybuilding</li>
+                    <li>Powerlifting</li>
+                    <li>Jujitsu</li>
+                </ul>
+            </div> 
           </div>
       </section>
+    </>
   )
 }
