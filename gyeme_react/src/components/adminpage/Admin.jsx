109f07faf185215af69e138076b229c537a178f3
Nathan <nathan.ewan14@gmail.com>
2022-02-03 13:43:44 +1000

    Adding a useState for users and an addUsers function

--- gyeme_react/src/components/adminpage/Admin.jsx
+++ gyeme_react/src/components/adminpage/Admin.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import UserList from './UserList';
+import { useState } from 'react';
+import './Admin.css';
+import { useNavigate } from 'react-router-dom';
 
 export default function Admin() {
     const [users, setUsers] = useState([
@@ -20,8 +23,27 @@ export default function Admin() {
         }
     ])
 
+    const navigate = useNavigate()
+
+    const addUser = (username, password, role) => {
+        const newUser = {
+            id: 4,
+            name: username,
+            password: password,
+            role: role,
+        }
+        const newUsers = [...users, newUser]
+        setUsers(newUsers)
+    }
+
   return(
       <div>
+          <div className='titles'>
+            <h1>Gym Users</h1>
+            <button onClick={() => navigate('/users/new')}>
+                Create New Users
+            </button>
+          </div>
           <UserList users={users}/>
       </div>
   )
