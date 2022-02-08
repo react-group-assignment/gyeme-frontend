1de6f9df98604c1931178bca9646ef20962fe2b0
Nathan <nathan.ewan14@gmail.com>
2022-02-09 09:51:16 +1000

    Added useContext for getting array of users

--- gyeme_react/src/components/adminpage/UserList.jsx
+++ gyeme_react/src/components/adminpage/UserList.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import User from './User';
+import gymContext from '../../gymContext';
+import { useContext } from 'react';
 
-export default function UserList({ users }) {
+export default function UserList() {
+  const { state: { users } } = useContext(gymContext);
+
   return(
     <div>
       { users.map((user) => (
         <User
+          id={user.id}
           name={user.name}
           role={user.role}
         />
