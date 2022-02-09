90b52a52a7c6ad5d963a806bd1f916fec222d559
Nathan <nathan.ewan14@gmail.com>
2022-02-09 10:00:43 +1000

    Added delete user function using dispatch and imported useContext

--- gyeme_react/src/components/adminpage/User.jsx
+++ gyeme_react/src/components/adminpage/User.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './User.css'
+import { AiTwotoneDelete } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
+import gymContext from '../../gymContext';
 
 
-export default function User({ name, role }) {
+
+export default function User({ id, name, role }) {
+  const navigate = useNavigate();
+  const { state: { users }, dispatch } = useContext(gymContext);
+  
+
+  function alertFunction() {
+    if (confirm("Do you want to delete this user?")) {
+      deleteUser
+    } else {
+      console.log('canceled')
+    }
+  }
+
+  function deleteUser() {
+    const index = users.findIndex(u => u.id === id);
+    dispatch({
+      type: "deleteUser",
+      payload: index
+    })
+  }
+
   return(
     <div className='user'>
       <h3>{name} - {role}</h3>
-      <button>Update</button>
+      <div className='user-crud-options'>
+        <button onClick={() => navigate("/users/new")}>Update</button>
+        <div onClick={deleteUser}><AiTwotoneDelete></AiTwotoneDelete></div>
+      </div>
     </div>
   )
 }
