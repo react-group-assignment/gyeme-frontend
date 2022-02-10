11266df880f4a45816622beacaf379456dec17a8
Nathan <nathan.ewan14@gmail.com>
2022-02-10 13:23:18 +1000

    Added a statereducer and cases for adding and deleting

--- gyeme_react/src/stateReducer.js
+++ gyeme_react/src/stateReducer.js
@@ -1,11 +1,22 @@
 export default function (currentState, action) {
     switch (action.type) {
+        case 'setRoles':
+            return {
+                ...currentState,
+                roles: action.data
+            }
+        case 'setUsers':
+            return {
+                ...currentState,
+                users: action.data
+            }
         case "addUser":
             const newUser = {
                 id: 4,
-                name: action.username,
+                username: action.username,
+                email: action.email,
                 password: action.password,
-                role: action.role
+                role_id: action.role
             }
             return {
                 ...currentState,
@@ -15,6 +26,11 @@ export default function (currentState, action) {
             return {
                 ...currentState,
                 users: currentState.users.filter((user, index) => index !== action.payload)
+            }
+        case "editUser":
+            return {
+                ...currentState,
+                users: [...currentState.users.filter(u => u.id !== action.payload.id), action.payload]
             }
     }
 }
