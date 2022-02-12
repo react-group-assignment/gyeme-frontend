1cc1c96801dfe50e57f9af80451bdd9ce839eb74
Nathan <nathan.ewan14@gmail.com>
2022-02-13 09:05:05 +1000

    Rewrote code for user tests

--- src/components/adminpage/User.test.jsx
+++ src/components/adminpage/User.test.jsx
@@ -1,17 +1,28 @@
-import '@testing-library/jest-dom'
-import { describe, expect, it, beforeEach } from "vitest";
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { describe, expect, it } from 'vitest';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {render, fireEvent, screen} from '@testing-library/react';
 import User from './User'
-
 
 describe('App components tests', () => {
 
     beforeEach(() => {
-        render(<User/>)
+        render(
+            <Router>
+                <Routes>
+                    <Route
+                        //to="/admin"
+                        element={<User/>}
+                    />
+                </Routes>
+            </Router>
+        )
     })
 
-    it('shows heading', () => {
-        expect(screen.getByRole('heading', {name: 'Gym Users'})).toBeInTheDocument()
+    it('shows button', () => {
+        expect(screen.findByRole('button', {name: 'Update'}))
     })
+
+    // it('shows button', () => {
+    //     expect(screen.findByRole('delete-icon'))
+    // })
 })
